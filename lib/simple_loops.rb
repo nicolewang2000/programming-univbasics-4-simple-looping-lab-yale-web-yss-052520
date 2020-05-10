385d03505a129356a17647b1008f9eaf5cd7d17c
Nicole Wang <nicole.wang@yale.edu>
2020-05-10 19:35:35 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  n = 5
+  while n >= 1 do
+    puts message
+    n -= 1
+  end
+end 
+
+def loop_message_n_times(message, n)
+  while n >= 1 do
+    puts message
+    n -= 1
+  end
+end 
+
+def output_array(array)
+  n = 0 
+  while n < array.length do 
+    puts array[n]
+    n += 1 
+  end
+end
+
+def return_string_array(array)
+  n = 0 
+  while n < array.length do
+    array[n] = array[n].to_s
+    n += 1
+  end
+  array
+end
